e65c50848c6efd35bae3ff7cff847780e49b6e83
Rishabh705 <211131@iiitt.ac.in>
2022-10-03 19:08:15 +0530

    Updated main(), park().

--- park.cpp
+++ park.cpp
@@ -10,40 +10,37 @@ class ParkingEntry
 protected:
     vector<vector<string>> slots;
     vector<int> empty_slots;
-    int nearestSlot(vector<int> empty)
-    {
-        return min_element(empty.begin(), empty.end()) - empty.begin();
-    }
-
 public:
     ParkingEntry(int n = 10)
     {
         for (size_t i = 0; i < n; i++)
-        {
             empty_slots.push_back(i); // as initially all slots are empty.
-        }
+        for (int i = 0; i < 3; i++)
+            slots.push_back(vector<string>()); // Add an empty row
     }
     void park(string n = "", string clr = "") // park car of particular color = clr  and number = n at nearest slot.
     {
-        int nearest = nearestSlot(empty_slots); // nearest available slot obtained
+        int nearest = *min_element(empty_slots.begin(), empty_slots.end()); // nearest available slot obtained
         slots[0].push_back(clr);
         slots[1].push_back(n);
         slots[2].push_back(to_string(nearest));
         auto it = find(empty_slots.begin(), empty_slots.end(), nearest);
         int index = it - empty_slots.begin();
         empty_slots.erase(empty_slots.begin() + index);
+        cout << "Your car is parked at slot number " << nearest + 1 << endl;
         j++;
     }
-    void unpark(string n, string clr)
+    void unpark(string n)
     {
         bool car_was_present = false;
         int i;
         for (i = 0; i < j; i++)
         {
-            if (clr.compare(slots[0][i]) == 0 && n.compare(slots[1][i]) == 0)
+            if (n.compare(slots[1][i]) == 0)
             {
                 slots[0].erase(slots[0].begin() + i);
                 slots[1].erase(slots[1].begin() + i);
+                cout << "Your car is unparked!\n";
                 car_was_present = true;
                 j--;
                 break;
@@ -110,58 +107,61 @@ public:
         }
     }
 };
-int main(int argc, char *argv[])
+int main()
 {
-    string filename = argv[1];
-    string extension = filename.substr(filename.length() - 4);
-    if (extension.compare("txt")) // file operation
+    int n,res;
+    cout << "Enter capacity of Parking lot : ";
+    cin >> n;
+    ParkingEntry obj(n);
+    Details ob;
+    do
     {
-    }
-    else
-    {
-        if (strcmp(argv[1], "create_parking_lot") == 0)
+        cout << "\n1.Park car.\n";
+        cout << "2.Unpark car.\n";
+        cout << "3.Find slot.\n";
+        cout << "4.Find car number.\n";
+        cout << "5.Find car.\n";
+        cout << "Enter your choice : ";
+        int slot;
+        cin >> res;
+        string reg, clr;
+        if (res == 1)
         {
-            int n = stoi(argv[2]);
-            ParkingEntry obj(n);
-            for (size_t i = 3; i < argc;)
-            {
-                if (argv[i] == "park")
-                {
-                    obj.park(argv[i + 1], argv[i + 2]);
-                    i += 3;
-                }
-                else if (argv[i] == "unpark")
-                {
-                    obj.unpark(argv[i + 1], argv[i + 2]);
-                    i += 3;
-                }
-                else if (argv[i] == "find_parking_slot")
-                {
-                    Details ob;
-                    ob.findCar("", stoi(argv[i + 1]), false);
-                    i += 2;
-                }
-                else if (argv[i] == "find_car_number")
-                {
-                    Details ob;
-                    ob.findCar(argv[i + 1], 0, true);
-                    i += 2;
-                }
-                else if (argv[i] == "find_car")
-                {
-                    Details ob;
-                    ob.findByClr(argv[i + 1]);
-                    i += 2;
-                }
-                else
-                {
-                    cout << "Error : Entered command is inappropriate\n";
-                    break;
-                }
-            }
+            cout << "\nEnter car number : ";
+            cin >> reg;
+            cout << "\nEnter car colour : ";
+            cin >> clr;
+            obj.park(reg, clr);
+        }
+        else if (res == 2)
+        {
+            cout << "\nEnter car number : ";
+            cin >> reg;
+            obj.unpark(reg);
+        }
+        else if (res == 3)
+        {
+            cout << "\nEnter slot number : ";
+            cin >> slot;
+            ob.findCar("", slot, false);
+        }
+        else if (res == 4)
+        {
+            cout << "\nEnter car number : ";
+            cin >> reg;
+            ob.findCar(reg, 0, true);
+        }
+        else if (res == 5)
+        {
+            cout << "\nEnter car colour : ";
+            cin >> clr;
+            ob.findByClr(clr);
         }
         else
-            cout << "Error : Inappropriate command or Parking Lot not created\n";
-    }
+        {
+            cout << "Error : Entered input is invalid\n";
+        }
+    } while (res!=0);
+    cout << "Thank You\n";
     return 0;
 }
